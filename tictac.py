2edd4a7a8bc6bcd0a49cd54a143c802467bcba65
banshee <blanche.jamail@gmail.com>
2014-03-23 23:21:11 -0500

    sunday draft. chararray -> nested list

--- tictac.py
+++ tictac.py
@@ -1,53 +1,46 @@
-import numpy as np
-board = np.chararray((3,3))
-board[:] = '_'
+
+
+#function for player X's move
+#need to review indents, do not seem to understand yet (many unindent errors)
+def yourmove(board):
+  row = raw_input("Which row would you like to choose? TOP, MIDDLE, or BOTTOM? ")
+  col = raw_input("Which column do you choose? LEFT, CENTER, or RIGHT? ")
+	if row == "TOP":
+	  row == 0
+	elif row == "MIDDLE":
+	  row == 1
+	elif row == "BOTTOM":
+	  row == 2
+	else:
+		row = "ERROR. Please indicate TOP, MIDDLE, or BOTTOM row"
+#column    
+	if col == "LEFT":
+	  col == 0
+	elif col == "CENTER":
+	  col == 1
+	elif col == "RIGHT":
+	  col == 2
+	else: 
+	  col == "ERROR. Please indicate LEFT, CENTER, OR RIGHT column"  
+
+  
+board[row][col] = 'X'
+print board
+
+def compturn(n):
+  row = np.random.randint(3)
+  col = np.random.randint(3)#should I change variable names in this function or keep row/cow
+  board[row][col] = 'O'
+  print board
+
+def main():
+  import numpy as np
+  board = [ [()]*3 for i in range(3) ]
+  print board
 print "I challenge you to a game of TIC TAC TOE"
 print "You are player X: Your move first!"
 
-#FIND LOCATION FOR X function
-#make functions for x turn o turn
+yourmove(board)
+compturn(board)
 
-x = raw_input("Which row would you like to choose? TOP, MIDDLE, or BOTTOM? ")
-y = raw_input("Which column do you choose? LEFT, CENTER, or RIGHT? ")
-
-if x == "TOP":
-  row = 0
-elif x == "MIDDLE":
-  row = 1
-elif x == "BOTTOM":
-  row = 2
-else:
-	row = "ERROR"
-    
-if y == "LEFT":
-  col = 0
-elif y == "CENTER":
-  col = 1
-elif y == "RIGHT":
-  col = 2
-else: 
-  col = "ERROR"
-
-for i in np.nditer(board):
-  if i == board[row,col]:
-    board[row,col] = 'X'
-print board
- #need to make sure new board in place with X marked spot before proceding to computer turn
-
-row = np.random.randint(3)
-col = np.random.randint(3)#should I change variable names in this function or keep row/col?
-for i in np.nditer(board):
-  if board[row,col] == '_':
-    board[row,col] = 'O'
-  elif board != '_':
-    row = np.random.randint(3)#how do I account for X or O already on board? look back and change int
-    col = np.random.randint(3)
-print board
-
-
-
-#O'S RANDOM _ REPLACED BY 'COMPUTER' FUNcTION
-
-
-
-#PRINT BOARD, BACK TO USER CHOICE
+main()
